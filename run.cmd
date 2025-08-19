bf2c982c446c41ae044c23bfdd0e5fc2e724a52e
zarigata <cpuvalle@gmail.com>
2025-08-19 20:35:07 -0300

    REFRACTOR

--- run.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist .venv (python -m venv .venv)
-call .venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python -m app.main
--- run.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-if (-not (Test-Path ".venv")) { python -m venv .venv }
-. .\.venv\Scripts\Activate.ps1
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python -m app.main
